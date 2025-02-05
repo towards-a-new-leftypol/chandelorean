6a557c7c5d3f310923892b83505ed03c467fd10b
towards-a-new-leftypol <paul_cockshott@protonmail.com>
2025-02-04 23:44:38 -0500

    Sync - ensure we have all the boards

--- src/Sync.hs
+++ src/Sync.hs
@@ -2,11 +2,17 @@
 
 module Sync where
 
+import System.Exit (exitFailure)
+import qualified Data.Map as Map
+import qualified Data.Set as Set
+-- import Control.Monad.Trans.Except (ExceptT, runExceptT)
+
 import qualified Common.Server.ConsumerSettings as S
 import qualified Common.Server.JSONSettings as JS
 import qualified Network.DataClient as Client
 import qualified Lib
-import Control.Monad.Trans.Except (ExceptT, runExceptT)
+import qualified Network.GetLatestPostsPerBoardResponse as GLPPBR
+import qualified SitesType as Site
 
 consumerSettingsToPartialJSONSettings :: S.ConsumerJSONSettings -> JS.JSONSettings
 consumerSettingsToPartialJSONSettings S.ConsumerJSONSettings {..} =
@@ -31,9 +37,44 @@ syncWebsites consumer_settings = do
 
     print sites
 
-    latest_posts_per_board <- Client.getLatestPostsPerBoard json_settings
+    latest_posts_per_board_results <- Client.getLatestPostsPerBoard json_settings
 
-    print latest_posts_per_board
+    case latest_posts_per_board_results of
+        Left e -> do
+            putStrLn $ "Error getting board information: " ++ show e
+            exitFailure
+        Right latest_posts_per_board -> do
+            print latest_posts_per_board
+
+            let boards_per_site :: Map.Map Int [ String ] =
+                    foldl
+                        (\m b ->
+                            let key = GLPPBR.site_id b
+                                pathpart = GLPPBR.pathpart b
+                            in
+
+                            Map.insertWith (++) key [ pathpart ] m
+                        )
+                        Map.empty
+                        latest_posts_per_board
+
+            let site_name_to_site_id :: Map.Map String Int = Map.fromList $ map (\s -> (Site.name s, Site.site_id s)) sites
+
+            mapM_
+                (\site_settings ->
+                    let s_id = (Map.!) site_name_to_site_id (S.name site_settings) in
+
+                    Lib.createArchivesForNewBoards
+                        (Lib.toClientSettings consumer_settings site_settings)
+                        (Set.fromList $ S.boards site_settings)
+                        ((Map.!) boards_per_site s_id)
+                        s_id
+                )
+                (S.websites consumer_settings)
+
+    -- we have our boards last modified timestamps
+    -- get list of boards per site
+
     -- first we need all the (Site, Board) tuples ✓
     -- perhaps we even want all (Site, Board, Thread) ✓
     -- But then we don't load the posts of each thread, instead only do
