456a76046985f7694293c9e1f62a063131fe86e3
towards-a-new-leftypol <paul_cockshott@protonmail.com>
2025-02-10 06:05:12 -0500

    WiP: new version of ensureThreads

--- src/Lib2.hs
+++ src/Lib2.hs
@@ -10,7 +10,10 @@ import qualified Network.DataClient as Client
 import qualified SitesType  as Sites
 import qualified BoardsType as Boards
 import Common.Network.HttpClient (HttpError)
-import JSONParsing (Catalog)
+import JSONParsing (Catalog, Thread)
+import qualified ThreadType as Threads
+import Common.Server.JSONSettings (JSONSettings)
+import qualified Lib
 
 data ProgramException = HttpException HttpError
   deriving Show
@@ -26,3 +29,17 @@ getCatalogJSON site board =
   where
     path = Boards.pathpart board </> "catalog.json"
     req = Client.getJSON $ Sites.url site </> path
+
+
+ensureThreads
+    :: JSONSettings
+    -> Boards.Board
+    -> [ Thread ]
+    -> ExceptT ProgramException IO [ Threads.Thread ]
+ensureThreads settings web_threads = do
+    db_threads <- Client.getThreads settings (Boards.board_id board) (map no web_threads)
+
+    -- save new threads
+    Client.postThreads
+        settings
+        (map (Lib.apiThreadToArchiveThread board_id) threads_to_create)
