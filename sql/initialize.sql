66c8a2a02afa9b695ec41dfa135376d2bedfc9bb
towards-a-new-leftypol <paul_cockshott@protonmail.com>
2025-03-14 02:31:31 -0400

    SQL: Alter user to set pgrst.db_aggregates_enabled
    - fix syntax error in initialize.sql

--- sql/initialize.sql
+++ sql/initialize.sql
@@ -470,8 +470,8 @@ GRANT EXECUTE ON FUNCTION get_latest_posts_per_board TO chan_archive_anon;
 -- GRANT usage, select ON SEQUENCE boards_board_id_seq TO chan_archive_anon;
 GRANT chan_archive_anon                 TO admin;
 
-CREATE ROLE chan_archiver noinherit login password 'test_password'
-    SET pgrst.db_aggregates_enabled = 'true';
+CREATE ROLE chan_archiver noinherit login password 'test_password';
+ALTER ROLE chan_archiver SET pgrst.db_aggregates_enabled = 'true';
 GRANT CONNECT ON DATABASE chan_archives TO chan_archiver;
 GRANT chan_archive_anon                 TO chan_archiver;
 GRANT ALL ON sites                      TO chan_archiver;
