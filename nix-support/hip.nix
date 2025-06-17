bb516285c346846750055acbd10fea80c8a0ed42
towards-a-new-leftypol <paul_cockshott@protonmail.com>
2025-06-16 23:17:01 -0400

    Removed building custom version of repa dependency
    - seems to work fine using upstream

--- nix-support/hip.nix
+++ nix-support/hip.nix
@@ -1,13 +1,15 @@
 { nixpkgs ? import <nixpkgs> {} }:
 
 let
+  /*
   haskellPackages = nixpkgs.haskellPackages.override {
     overrides = self: super: {
       repa = import ./repa.nix { inherit nixpkgs; };
     };
   };
+  */
 
-  hip = haskellPackages.callCabal2nix "hip" (nixpkgs.fetchFromGitHub {
+  hip = nixpkgs.haskellPackages.callCabal2nix "hip" (nixpkgs.fetchFromGitHub {
     owner = "lehins";
     repo = "hip";
     rev = "9f1111ea8e3f6d284404074cb6ac3e2ff164f0fe";
